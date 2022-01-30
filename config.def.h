fff75def6c29f76c14e3fe78ae5c300ebb71156a
Muja Mo Ismail <muja@foss.land>
2022-01-30 03:50:54 +0100

    changed tags to icons && rules launch some apps in specific tag

--- config.def.h
+++ config.def.h
@@ -19,16 +19,19 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class          instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",         NULL,       NULL,       0,            1,           -1 },
+	{ "Emacs",        NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Thunderbird",  NULL,       NULL,       1 << 6,       0,           -1 },
+	{ "Steam",        NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "Firefox",      NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
