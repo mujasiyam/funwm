69510365f4fb80f80bb66550674bbdd98440e9e8
Muja Mo Ismail <muja@foss.land>
2022-01-31 01:54:49 +0100

    change color scheme

--- config.def.h
+++ config.def.h
@@ -7,11 +7,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "firacoderetina:size=10" };
 static const char dmenufont[]       = "firacoderetina:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#2D2A2E";
+static const char col_gray2[]       = "#a9dc76";
+static const char col_gray3[]       = "#ffd866";
+static const char col_gray4[]       = "#a9dc76";
+static const char col_cyan[]        = "#2D2A2E";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
